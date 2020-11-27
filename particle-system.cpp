cae5b210358269ea35cc8afded8894ab317635a9
Henry Rubiano-Poveda <henry.rubianopoveda@student.manchester.ac.uk>
2020-11-27 16:30:26 +0000

    First try of explosions, not working

--- particle-system.cpp
+++ particle-system.cpp
@@ -33,10 +33,11 @@ int LAUNCH_RANGE = 80;
 
 // Number of particles in the array (they will be reused
 // and recycled when dead so it seems that there are much more)
-const int particleLength = 20;
+const int particleLength = 200;
+const int mainParticles = particleLength / 10;
 
 // Forces that will act as acceleration in multiple dimensions
-GLfloat gravity = -0.01;         
+GLfloat gravity = -0.01;
 
 
 double myRandom();
@@ -62,46 +63,25 @@ public:
   // LifeLength
   GLint lifeLength;
 
+  // Parent index
+  int parentIdx;
+
 };
 
+// Array of all particles
 Particle particles[particleLength];
 
-///////////////////////////////////////////////
-// Method to initialise a single particle of the array
-
-void initParticle(int index) {
-
-  // Initial color of the particle -- black at the moment
-  particles[index].r = 0.0;
-  particles[index].g = 0.0;
-  particles[index].b = 0.0;
-
-  // This defines the area of possible launch:
-  // (-LAUNCH_RANGE/2, -LAUNCH_RANGE/2) to (LAUNCH_RANGE/2, LAUNCH_RANGE/2)
-  particles[index].x = myRandom() * LAUNCH_RANGE - (LAUNCH_RANGE / 2);
-  particles[index].y = 0.0;
-  particles[index].z = myRandom() * LAUNCH_RANGE - (LAUNCH_RANGE / 2);
-
-  // printf("(%f, %f)\n", particles[index].x, particles[index].z);
-
-  // Initial velocity of the particle when launched 
-  particles[index].vx = 0;
-  particles[index].vy = myRandom() * 0.3 + 0.7;   
-  particles[index].vz = 0;
-
-  // printf("%f\n", particles[index].vy);
-
-  // Size, and time alive (if negative, time to be reborn)
-  particles[index].size = 1;
-  particles[index].lifeLength = 0;
-
-}
+// Method declaration
+void initParticle(int index);
+void initExplosionParticle(int index);
+void updateParticle(int index);
+void updateExplosionParticle(int index);
 
 ///////////////////////////////////////////////
-// Initialise all the particles in the array
+// Initialise all the main particles in the array
 
 void initParticles() {
-  for (int index = 0; index < particleLength; index++) {
+  for (int index = 0; index < mainParticles; index++) {
     initParticle(index);
   }
 }
@@ -161,15 +141,27 @@ void display()
   // If enabled, draw coordinate axis
   if(axisEnabled) glCallList(axisList);
 
+  // Starting idx for the children to be checked
+  int childrenToCheck = 0;
+
   // Here, we will display the particle, modified according
   // to the translations and colour changes.
-  for(int index = 0; index < particleLength; index ++) {
+  for(int index = 0; index < particleLength / 10; index ++) {
     if (particles[index].lifeLength >= 0) {
       glPushMatrix();
       glTranslatef(particles[index].x, particles[index].y, particles[index].z);
       glColor3f(particles[index].r, particles[index].g, particles[index].b);
       glutSolidSphere(particles[index].size, 150, 150);
       glPopMatrix();
+    } else {
+      childrenToCheck = (index + particleLength / 100) * 10;
+      for (int i = childrenToCheck; i < childrenToCheck + 10; i++) {
+        glPushMatrix();
+        glTranslatef(particles[i].x, particles[i].y, particles[i].z);
+        glColor3f(particles[i].r, particles[i].g, particles[i].b);
+        glutSolidSphere(particles[i].size, 150, 150);
+        glPopMatrix();
+      }
     }
   }
 
@@ -214,29 +206,9 @@ void animations()
 {
 
   // Loop through all particles
-  for (int index = 0; index < particleLength; index++) {
-
-    // Time passes
+  for (int index = 0; index < particleLength / 10; index++) {
     particles[index].lifeLength++;
-
-    // If time to be born
-    if (particles[index].lifeLength == 0) {
-      initParticle(index);
-
-      // If alive
-    } else if (particles[index].lifeLength > 0) {
-
-      // Updating position and velocity of y direction
-      // (Only shooting upwards for now)
-      
-      particles[index].y += particles[index].vy;
-      particles[index].vy += gravity;
-
-      // Time to die
-      if (particles[index].vy < 0) particles[index].lifeLength = myRandom() * 100 - 100;
-
-    }
-
+    updateParticle(index);
   }
 
   glutPostRedisplay();
@@ -289,4 +261,117 @@ int main(int argc, char *argv[])
   initGraphics(argc, argv);
   glEnable(GL_POINT_SMOOTH);
   glutMainLoop();
+}
+
+
+//////////////////////////////////////////////////
+// Methods for PARTICLES
+
+void initParticle(int index) {
+
+  // Initial color of the particle -- black at the moment
+  particles[index].r = 0.0;
+  particles[index].g = 0.0;
+  particles[index].b = 0.0;
+
+  // This defines the area of possible launch:
+  // (-LAUNCH_RANGE/2, -LAUNCH_RANGE/2) to (LAUNCH_RANGE/2, LAUNCH_RANGE/2)
+  particles[index].x = myRandom() * LAUNCH_RANGE - (LAUNCH_RANGE / 2);
+  particles[index].y = 0.0;
+  particles[index].z = myRandom() * LAUNCH_RANGE - (LAUNCH_RANGE / 2);
+
+  // printf("(%f, %f)\n", particles[index].x, particles[index].z);
+
+  // Initial velocity of the particle when launched
+  particles[index].vx = myRandom() * 0.1 - 0.05;
+  particles[index].vy = myRandom() * 0.3 + 0.7;
+  particles[index].vz = myRandom() * 0.1 - 0.05;
+
+  // printf("%f\n", particles[index].vy);
+
+  // Size, and time alive (if negative, time to be reborn)
+  particles[index].size = 1;
+  particles[index].lifeLength = 0;
+
+}
+
+
+//Method to initialise the explosion particle
+void initExplosionParticle(int index) {
+
+  particles[index].parentIdx = (index - particleLength / 10) / 10;
+
+  // Initial color of the particle -- red for explosion particles
+  particles[index].r = 1.0;
+  particles[index].g = 0.0;
+  particles[index].b = 0.0;
+
+  // Parent area
+  particles[index].x = particles[particles[index].parentIdx].x;
+  particles[index].y = particles[particles[index].parentIdx].y;
+  particles[index].z = particles[particles[index].parentIdx].z;
+
+  // printf("(%f, %f)\n", particles[index].x, particles[index].z);
+
+  // Initial velocity of the particle when launched
+  particles[index].vx = myRandom() * 0.01 - 0.005;
+  particles[index].vy = myRandom() * 0.03 + 0.07;
+  particles[index].vz = myRandom() * 0.01 - 0.005;
+
+  // printf("%f\n", particles[index].vy);
+
+  // Size, and time alive (if negative, time to be reborn)
+  particles[index].size = 1;
+  particles[index].lifeLength = -100;
+
+}
+
+// Method to update the particle
+void updateParticle(int index) {
+
+  // If time to be born
+  if (particles[index].lifeLength == 0) {
+    initParticle(index);
+
+    // If alive
+  } else if (particles[index].lifeLength > 0) {
+
+    // Updating position and velocity
+    particles[index].x += particles[index].vx;
+    particles[index].y += particles[index].vy;
+    particles[index].z += particles[index].vz;
+    particles[index].vy += gravity;
+
+    // Time to die
+    if (particles[index].vy < 0) {
+
+      // Seed disappears
+      particles[index].lifeLength = myRandom() * 100 - 100;
+
+      // Explosion particles appear
+      for (int i = (index + particleLength / 100) * 10;
+            i < ((index + particleLength / 100) * 10) + 10;
+            i++) {
+        initExplosionParticle(i);
+      }
+
+      // If dead update its children
+    } else {
+      for (int i = (index + particleLength / 100) * 10;
+            i < ((index + particleLength / 100) * 10) + 10;
+            i++) {
+        updateExplosionParticle(i);
+      }
+    }
+  }
+}
+
+// Method for explosion particles
+void updateExplosionParticle(int index) {
+
+    particles[index].x += particles[index].vx;
+    particles[index].y += particles[index].vy;
+    particles[index].z += particles[index].vz;
+    particles[index].vy += gravity;
+
 }
